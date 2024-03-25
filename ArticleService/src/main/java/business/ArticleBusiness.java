fbcadbf1627e1578c6cda57cb4f342d3842161a5
Awedtan <31903483+Awedtan@users.noreply.github.com>
2024-03-24 22:32:58 -0400

    jwt subject validation

--- ArticleService/src/main/java/business/ArticleBusiness.java
+++ ArticleService/src/main/java/business/ArticleBusiness.java
@@ -1,13 +1,17 @@
 package business;
 
-import persistence.*;
 import java.util.ArrayList;
-import helper.*;
+import java.util.Map.Entry;
+
+import helper.Article;
+import helper.ArticlesXML;
+import persistence.ArticleCRUD;
 
 public class ArticleBusiness {
     public boolean insertArticle(int uid, String title, String content, String token) {
         boolean response = false;
-        if (Authenticate.verifyToken(token).getKey()) {
+        Entry<Boolean, String> valid = Authenticate.verifyToken(token);
+        if (valid.getKey() && Integer.parseInt(valid.getValue()) == uid) {
             response = ArticleCRUD.insertArticle(uid, title, content);
         }
         return response;
